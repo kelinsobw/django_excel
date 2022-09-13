264095eb1b0a2f1f9c8ac91213b581377f3a7b8e
kelinso <kelinsobw@gmail.com>
2022-09-13 21:12:40 +0300

    Фильтрация взвешиваний 2

--- control/views.py
+++ control/views.py
@@ -1,4 +1,3 @@
-from json.decoder import NaN
 
 import pandas as pd
 import transliterate
@@ -13,11 +12,12 @@ def cabinet_weight(request, cabinet):
         data_now = read_file('all')
         head = read_file('head')
         cabinet_num = 0
+        weight_info = []
+        counter = [0, 1, 2, 3]
         for i in range(5, len(data_now[0])):
             if (transliterate.translit(((data_now[0][i]).lower().replace(" ", "")), reversed=True)) == cabinet:
                 cabinet_num = i
         cosmetic_in_cab = []
-        print(str(data_now[i][cabinet_num]))
         for i in range(1,len(data_now)):
             if str(data_now[i][cabinet_num]) != "nan":
                 cosmetic_in_cab.append((data_now[i][2],data_now[i][3],data_now[i][4],data_now[i][cabinet_num]))
@@ -27,13 +27,20 @@ def cabinet_weight(request, cabinet):
                 (str(transliterate.translit(((head[el]).replace(" ", "")), reversed=True)), str(head[el])))
         p_choice_room = forms.ChoiceField(choices=choice_room, label='')
 
+    if request.POST:
+        if '_formi' in request.POST:
+            print(("as________________as"))
+
     if request.method == "POST":
         form = Cabinet_weight(request.POST, request.FILES)
         if form.is_valid():
+            print("qwerty")
+            print(form.cleaned_data)
             info_base = form.cleaned_data
             return redirect(f"/weighing/{info_base.get('p_choice_room').lower()}", {"form": form})
     else:
         form = Cabinet_weight()
+        print("jskd")
         return render(request, "control/cabinet_weight.html", {"form": form})
 
 
